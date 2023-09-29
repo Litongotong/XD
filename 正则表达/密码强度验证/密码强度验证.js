73a76a9051dfaaea596cac66590f335bdcee2e93
Litongtong <li-t@i-sol.jp>
2023-09-29 11:20:08 +0900

    js案例：密码强度认证 active/正则表达/for

--- 正则表达/密码强度验证/密码强度验证.js
+++ 正则表达/密码强度验证/密码强度验证.js
@@ -1,17 +1,28 @@
-(() =>
-  function () {
-    const oninput = document.querySelector("input");
-    const reg1 = /\d/;
-    const reg2 = /[a-z]/i;
-    const reg3 = /[#$%^&*()]/;
+(() => {
+  //获取input
+  const myForm = document.querySelector("input");
 
-    oninput.oninput = function (evt) {
-      console.log(this.value);
-      // console.log(evt.target.value )
-      const level = 0;
-      if (reg1.test(this.value)) level++;
-      if (reg2.test(this.value)) level++;
-      if (reg3.test(this.value)) level++;
-      
-    };console.log(level);
-  })();
+  //定义三个强度的正则表达
+  const reg1 = /\d/;
+  const reg2 = /[a-z]/i;
+  const reg3 = /[#$%^&*()]/;
+
+  //绑定input事件
+  myForm.oninput = function (evt) {
+    // console.log(this.value);
+    // console.log(evt.target.value);
+    let level = 0;
+    if (reg1.test(evt.target.value)) level++;
+    if (reg2.test(evt.target.value)) level++;
+    if (reg3.test(evt.target.value)) level++;
+    console.log(level);
+    const ospan = document.querySelectorAll("span");
+    for (let i = 0; i < ospan.length; i++) {
+      ospan[i].classList.remove("active");
+      if (i < level) {
+        ospan[i].classList.add("active");
+        console.log(level);
+      }
+    }
+  };
+})();
